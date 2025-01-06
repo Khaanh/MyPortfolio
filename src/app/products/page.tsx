52e00886235fe4bc7b84345537facbc31b35555a
Khanh <Khanh13Nguyen13@gmail.com>
2025-01-06 23:54:10 +0100

    NextJS tutorial #7 -  Dynamic Routes

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+export default function ProductPage() {
+	return (
+		<>
+			<h1>Products Page</h1>
+			<h2>Product details 1</h2>
+			<h2>Product details 2</h2>
+			<h2>Product details 3</h2>
+			<h2>Product details 4</h2>
+		</>
+	);
+}
